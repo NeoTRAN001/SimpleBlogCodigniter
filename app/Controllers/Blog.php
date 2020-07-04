29b416c284a81dfe4f2d8dc4c2954f19e08a73bd
NeoTRAN <neo_tran@outlook.com>
2020-07-03 23:05:54 -0500

    Se ha agregado la vista, el modelo y el controlador de la creación de un blog

--- app/Controllers/Blog.php
+++ app/Controllers/Blog.php
@@ -1,5 +1,7 @@
 <?php namespace App\Controllers;
 
+use App\Models\BlogModel;
+
 class Blog extends BaseController
 {
     public function post($slug)
@@ -7,5 +9,30 @@ class Blog extends BaseController
         echo view('templates/header');
         echo view('blog/post');
         echo view('templates/footer');
-    } 
+    }
+
+    public function create()
+    {
+        helper('form');
+        $model = new BlogModel();
+
+        if(!$this->validate([
+          'title' => 'required|min_length[3]|max_length[255]',
+          'body'  => 'required'
+        ])) {
+            echo view('templates/header');
+            echo view('blog/create');
+            echo view('templates/footer');
+        } else {
+            $model->save(
+                    [
+                        'title' => $this->request->getVar('title'),
+                        'body' => $this->request->getVar('body'),
+                        'slug' => url_title($this->request->getVar('title')),
+                    ]
+                );
+
+            return redirect()->to('/Blog/public');
+        }
+    }
 }
